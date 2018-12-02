813e21981cb8027a9a211b1f9840b38a51e5c03d
Maurice Grela <maurice.grela@gmail.com>
2018-12-02 14:04:29 -0800

    Committing some fixes to the translations in the menu.

--- Assets/Scripts/UI/MenuChapterManager.cs
+++ Assets/Scripts/UI/MenuChapterManager.cs
@@ -39,7 +39,7 @@ public class MenuChapterManager : MonoBehaviour {
 
     public void FrenchStructure()
     {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < ButtonDescriptionText.Length; i++)
         {
             ButtonDescriptionText[i].text = Translations[i];//.ToString();
             //Debug.Log (transform.GetChild(i).name);
